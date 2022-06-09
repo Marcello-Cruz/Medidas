7be130c33bcd644dc616d26c044be6201f84fae9
Marcello-Cruz <marcelinhopastelzinho@gmail.com>
2022-06-09 20:37:45 -0300

    add exerc completin

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double m, cm, km;
+
+            Console.Write("Entre com a medida (em metros): ");
+            m = Convert.ToDouble(Console.ReadLine());
+
+            cm = m * 100;
+            km = m / 1000;
+
+            Console.WriteLine("\n--- Equivalência ---");
+            Console.WriteLine($"{cm} cm");
+            Console.WriteLine($"{m} m");
+            Console.WriteLine($"{km} Km");
+        }
+    }
+}
